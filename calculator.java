6e25bb34a89cd945b096feaac5a2baab5e5e16df
tsc221645 <tsc221645@uvg.edu.gt>
2023-04-06 12:28:50 -0600

    lol

--- calculator.java
+++ calculator.java
@@ -4,15 +4,7 @@ package simpleprojects;
 import java.util.Scanner;
 
 public class calculator {
-    // varible declarations
-    float num1;
-    float num2;
-    float ans;
-    String operator;
-    operator = " ";
-
-    //print statement
-    System.out.println("---------------------------");
-    System.out.println("        CALCULATOR        |");
-    System.out.println("---------------------------");
+    public static void main(String[] args){
+        
+    }
 }

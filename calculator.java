2b0df90fe7b5fb65ed57bedd1ffd7f4570fe75c9
tsc221645 <tsc221645@uvg.edu.gt>
2023-04-06 12:30:36 -0600

    variables

--- calculator.java
+++ calculator.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 
 public class calculator {
     public static void main(String[] args){
-        
+        // varible declarations
+        float num1;
+        float num2;
+        float ans;
+        String operator;
+        operator = " ";
+
+        //print statement
+        System.out.println("---------------------------");
+        System.out.println("        CALCULATOR        |");
+        System.out.println("---------------------------");
     }
 }
